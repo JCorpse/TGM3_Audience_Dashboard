276c77c083f6f34bf6ac6c75abea52b3947c6279
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-04-26 18:36:01 +0800

    change DISCORD_CHANNEl

--- src/main/java/com/jcorpse/tgm3/config/Constant.java
+++ src/main/java/com/jcorpse/tgm3/config/Constant.java
@@ -14,7 +14,7 @@ public class Constant {
 //    public static final String TWITCH_CHANNEL_ID = "146922206";
 
 
-    //    public static final String DISCORD_CHANNEl = "812636716640501760";
-    public static final String DISCORD_CHANNEl = "687712826978074675";
+    public static final String DISCORD_CHANNEl = "812636716640501760";
+    //    public static final String DISCORD_CHANNEl = "687712826978074675";
     public static final String DISCORD_TOKEN = System.getenv("Discord_TOKEN");
 }
