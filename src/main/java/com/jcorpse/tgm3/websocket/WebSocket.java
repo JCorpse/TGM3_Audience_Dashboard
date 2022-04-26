276ae59a17661d96530492bea001771421b909cd
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-04-26 17:07:03 +0800

    Test

--- src/main/java/com/jcorpse/tgm3/websocket/WebSocket.java
+++ src/main/java/com/jcorpse/tgm3/websocket/WebSocket.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.github.twitch4j.pubsub.domain.*;
+import com.github.twitch4j.pubsub.events.HypeTrainLevelUpEvent;
+import com.github.twitch4j.pubsub.events.HypeTrainProgressionEvent;
+import com.github.twitch4j.pubsub.events.HypeTrainStartEvent;
+import com.jcorpse.tgm3.config.Constant;
 import com.jcorpse.tgm3.dto.HyperTrain;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,6 +16,9 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Component
 @Slf4j
@@ -24,10 +32,18 @@ public class WebSocket extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        if(message.getPayload().equalsIgnoreCase("Jtest")){
-            BroadcastTest();
-        }else {
-            session.sendMessage(message);
+        switch (message.getPayload()){
+            case "starttest":
+                startTest();
+                break;
+            case  "ptest":
+                pTest();
+                break;
+            case  "lvtest":
+                lvTest();
+                break;
+            default:
+                session.sendMessage(message);
         }
     }
 
@@ -42,18 +58,49 @@ public class WebSocket extends TextWebSocketHandler {
         SessionManager.broadcast(message);
     }
 
-    private void BroadcastTest() throws JsonProcessingException {
-        HyperTrain Train = new HyperTrain();
-        Train.setTrainID("123456789");
-//        Train.setLastLevel(5);
-//        Train.setPercent("88");
-        Train.setExpiresAt("2022-04-23 23:26:39");
-        Train.setStartedAt("2022-04-23 23:31:39");
-//        Train.setEndAt("2022-04-23 23:31:39");
-//        Train.setGoal(1122348568);
-//        Train.setProgress(5566668);
+    private void startTest() throws JsonProcessingException {
+        HypeTrainStart hypeTrainStart = new HypeTrainStart();
+        hypeTrainStart.setId(UUID.randomUUID().toString());
+        hypeTrainStart.setStartedAt(Instant.now());
+        hypeTrainStart.setChannelId(Constant.TWITCH_CHANNEL_ID);
+        hypeTrainStart.setExpiresAt(Instant.now());
+        HypeTrainStartEvent startEvent = new HypeTrainStartEvent(hypeTrainStart);
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(Train);
+        String json = ow.writeValueAsString(startEvent);
+        wsBroadcast(new TextMessage(json));
+    }
+
+    private void pTest() throws JsonProcessingException {
+        HypeTrainProgress hypeTrainProgress = new HypeTrainProgress();
+        hypeTrainProgress.setGoal(18000);
+        hypeTrainProgress.setTotal(900000);
+        hypeTrainProgress.setValue(50);
+        hypeTrainProgress.setRemainingSeconds(50);
+        HypeProgression progression = new HypeProgression();
+        progression.setProgress(hypeTrainProgress);
+        progression.setAction("pTest");
+        progression.setQuantity(99);
+        progression.setSource("1122");
+        progression.setSequenceId(1123);
+        progression.setUserId("123");
+        HypeTrainProgressionEvent progressionEvent = new HypeTrainProgressionEvent(Constant.TWITCH_CHANNEL_ID,progression);
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(progressionEvent);
+        wsBroadcast(new TextMessage(json));
+    }
+
+    private void lvTest() throws JsonProcessingException {
+        HypeTrainProgress hypeTrainProgress = new HypeTrainProgress();
+        hypeTrainProgress.setGoal(18000);
+        hypeTrainProgress.setTotal(900000);
+        hypeTrainProgress.setValue(50);
+        hypeTrainProgress.setRemainingSeconds(50);
+        HypeLevelUp levelUp = new HypeLevelUp();
+        levelUp.setProgress(hypeTrainProgress);
+        levelUp.setTimeToExpire(Instant.now());
+        HypeTrainLevelUpEvent levelUpEvent = new HypeTrainLevelUpEvent(Constant.TWITCH_CHANNEL_ID,levelUp);
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(levelUpEvent);
         wsBroadcast(new TextMessage(json));
     }
 }
