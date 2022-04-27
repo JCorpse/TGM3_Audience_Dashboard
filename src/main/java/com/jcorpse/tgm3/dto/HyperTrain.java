b8fe4f2c34f2376133dc49fa3e511c9634b5cd46
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-04-27 18:00:18 +0800

    add HyperTrain serialVersionUID

--- src/main/java/com/jcorpse/tgm3/dto/HyperTrain.java
+++ src/main/java/com/jcorpse/tgm3/dto/HyperTrain.java
@@ -4,12 +4,16 @@ package com.jcorpse.tgm3.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HyperTrain implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 6870022363778741894L;
 
     private String TrainID;
     private String StartedAt;
