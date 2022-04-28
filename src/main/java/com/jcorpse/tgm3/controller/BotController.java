1f00fad060417fdb7c7c3226b1d88dcba3879712
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-04-28 11:03:03 +0800

    add bot RequestMapping

--- src/main/java/com/jcorpse/tgm3/controller/BotController.java
+++ src/main/java/com/jcorpse/tgm3/controller/BotController.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller(value = "/bot")
+@Controller
 @Slf4j
+@RequestMapping(value = "/bot")
 public class BotController {
 
     @GetMapping(value = "/send")
