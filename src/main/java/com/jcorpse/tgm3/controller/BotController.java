b009f3060ae76403cc0319120c46b0c1ba6a2aab
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-04-28 10:58:58 +0800

    rename botsend

--- src/main/java/com/jcorpse/tgm3/controller/BotController.java
+++ src/main/java/com/jcorpse/tgm3/controller/BotController.java
@@ -1,28 +1,26 @@
 package com.jcorpse.tgm3.controller;
 
 import com.jcorpse.tgm3.bot.twitch.TwitchBot;
-import com.jcorpse.tgm3.config.Constant;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@Controller(value = "/bot")
 @Slf4j
 public class BotController {
 
-    @GetMapping(value = "/botsend")
+    @GetMapping(value = "/send")
     public String getBotSend() {
-        return "botsend";
+        return "send";
     }
 
-    @PostMapping(value = "/botsend")
+    @PostMapping(value = "/send")
     public String Send(@RequestParam("token") String token,@RequestParam("msg") String msg) {
         if(token.equalsIgnoreCase(System.getenv("sendbot"))){
             TwitchBot.ChatSend(msg);
         }
-        return "botsend";
+        return "send";
     }
 }
