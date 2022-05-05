f8d06ca412a41a3d10d947950e1ec0b6d25a336b
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-05-05 16:14:09 +0800

    fix botsend return

--- src/main/java/com/jcorpse/tgm3/controller/BotController.java
+++ src/main/java/com/jcorpse/tgm3/controller/BotController.java
@@ -2,6 +2,8 @@ package com.jcorpse.tgm3.controller;
 
 import com.jcorpse.tgm3.bot.twitch.TwitchBot;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +19,10 @@ public class BotController {
 
     @CrossOrigin
     @PostMapping(value = "/send")
-    public void Send(@RequestParam("token") String token,@RequestParam("msg") String msg) {
+    public ResponseEntity Send(@RequestParam("token") String token,@RequestParam("msg") String msg) {
         if(token.equalsIgnoreCase(System.getenv("sendbot"))){
             TwitchBot.ChatSend(msg);
         }
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 }
